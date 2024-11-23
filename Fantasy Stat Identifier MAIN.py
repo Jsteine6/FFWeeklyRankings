439e6b85092e525e25e8e813df4c6dbb0728d979
Jsteine6 <Jsteine6@students.kennesaw.edu>
2024-11-22 21:09:20 -0500

    Update Fantasy Stat Identifier MAIN.py

--- Fantasy Stat Identifier MAIN.py
+++ Fantasy Stat Identifier MAIN.py
@@ -14,7 +14,7 @@ import bold
 BASE_URL = 'https://www.pro-football-reference.com{0}'
 PLAYER_LIST_URL = 'https://www.pro-football-reference.com/players/{0}'
 PLAYER_PROFILE_URL = 'https://www.pro-football-reference.com/players/{0}/{1}'
-PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}'
+PLAYER_GAMELOG_URL = 'https://www.pro-football-reference.com/players/{0}/{1}/gamelog/{2}/gamelog/{3}'
 
 class 
   
